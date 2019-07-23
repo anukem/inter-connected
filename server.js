23029ef97a013cf5befdd3d73824e6b58cf17975
Ezeugo Anukem <ezeugo@Ezeugos-MacBook-Pro.local>
2019-07-23 13:31:28 -0700

    commited port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ http.createServer(function (request, response)
 {
   response.writeHead(200, {"Content-Type": "text/plain"})
   response.end("this is my response to you")
-}).listen(process.env["APP_PORT"])
+}).listen(80)
 
 console.log('Server is running')
